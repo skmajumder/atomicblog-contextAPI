14d7d9e206118b5531e1c276176b6fa51994dcc7
skmajumder <skmajumder.cse@gmail.com>
2023-10-16 23:22:12 +0600

    throw new error for context api

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -3,6 +3,8 @@ import { PostContext } from "../contexts/PostProvider";
 
 const usePosts = () => {
   const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("PostContext was used outside of the PostProvider");
   return context;
 };
 
