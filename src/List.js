c9f3170b4fc98331ae1ac22014588b437728226f
skmajumder <skmajumder.cse@gmail.com>
2023-10-24 20:51:59 +0600

    Optimization Trick With children

--- src/List.js
+++ src/List.js
@@ -1,16 +1,21 @@
+// import Test from "./Test";
 import usePosts from "./hooks/usePosts";
 
 function List() {
   const { posts } = usePosts();
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      {/* <Test /> */}
+    </>
   );
 }
 
